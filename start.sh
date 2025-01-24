e0f61cdec0e7fcdd8d8f70b857098a3df396cd58
jobgen.ai <jobgenai.startup@gmail.com>
2025-01-24 18:33:55 +1100

    Update start.sh
    v2

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# Set environment variables
-export FLASK_APP=extract_text_api.py
-export FLASK_ENV=production
-
-# Ensure the custom temp directory exists
-mkdir -p ~/my_temp_files
-
-# Start the Flask app
-echo "Starting the Flask server..."
-flask run --host=0.0.0.0 --port=5000
+echo "Starting the Flask app..."
+gunicorn -b 0.0.0.0:5000 extract_text_api:app
